af50ac64bd15e29c376b6f9b8378fe5a5cc24893
Seth Vizel <seth.vizel@gmail.com>
2022-10-27 12:00:19 -0400

    Ticket constructor needs to be fixed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Lottery powerBall = new Lottery("PowerBall", 5, 69, 26);
+        ArrayList<Ticket> tickets = new ArrayList<>();
         Ticket winner = new Ticket(powerBall);
-        Ticket luckyTicket = new Ticket(powerBall);
 
+        Scanner scan = new Scanner(System.in);
+
+        Ticket notQP = new Ticket(powerBall, scan.nextLine());
         System.out.println(winner.ticketStr());
-        System.out.println(luckyTicket.ticketStr());
-
-        System.out.println(luckyTicket.compareTo(winner));
+        System.out.println(notQP.ticketStr());
+        System.out.println(notQP.compareTo(winner));
     }
 }
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ticket {
     private final Lottery game;
@@ -9,9 +10,14 @@ public class Ticket {
         numbers = game.draw();
     }
 
-    Ticket(Lottery game, ArrayList<Integer> numbers) {
+    Ticket(Lottery game, String numberStr) {
         this.game = game;
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>();
+
+        for (String num : numberStr.trim().split(", ")) {
+            numbers.add(Integer.valueOf(num));
+        }
+        Collections.sort(numbers);
     }
 
     public Lottery getGame() {return game;}
@@ -22,14 +28,14 @@ public class Ticket {
 
         int matches = 0;
         ArrayList<Integer> compareNumbers = compareTicket.getNumbers();
-        int regNums = numbers.size();
+        int regNums = numbers.size() - 1;
 
         for (int i = 0; i < regNums-1; i++) {
             if (Lottery.checkFor(numbers.get(i), compareNumbers)) {
                 matches++;
             }
         }
-        if (numbers.get(regNums) == compareNumbers.get(regNums)) {matches += 2;}
+        if (numbers.get(regNums).equals(compareNumbers.get(regNums))) {matches += 2;}
 
         return matches;
     }
@@ -37,7 +43,7 @@ public class Ticket {
     public String ticketStr() {
         StringBuilder numStr = new StringBuilder();
         for (Integer number : numbers) {
-            numStr.append(number).append(", ");
+            numStr.append(number < 10 ? "0" : "").append(number).append(", ");
         }
         return numStr.toString();
     }
